d57e79ac8fa0747f1bb8d72b90e155ae26a73118
Thomas Wiest <t.wiest@reply.de>
2022-05-05 08:43:19 +0200

    fixed maven configuration and added stream processor

--- WeatherKafkaConsumer/src/main/kotlin/lab/weather/consumer/StreamConsumer.kt
+++ WeatherKafkaConsumer/src/main/kotlin/lab/weather/consumer/StreamConsumer.kt
@@ -1,27 +1,38 @@
 package lab.weather.consumer
 
-import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import lab.weather.AggregatedWeatherInformationEvent
 import lab.weather.LabConfiguration
 import lab.weather.WeatherInformationEvent
-import org.apache.kafka.common.requests.MetadataRequest.Builder.allTopics
-import org.apache.kafka.common.serialization.Serdes
+import lab.weather.WeatherStationEvent
 import org.apache.kafka.streams.StreamsBuilder
-import org.apache.kafka.streams.kstream.Consumed
 import org.apache.kafka.streams.kstream.KStream
-import org.apache.kafka.streams.kstream.Predicate
+import org.apache.kafka.streams.kstream.KTable
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import java.util.*
 
 
 @Component
-class StreamConsumer(val labConfiguration: LabConfiguration) {
+class StreamConsumer(private val labConfiguration: LabConfiguration) {
 
     @Autowired
     fun buildPipeline(streamsBuilder: StreamsBuilder) {
+        val stationKTable: KTable<String, WeatherStationEvent> =
+            streamsBuilder.stream<String, WeatherStationEvent>(labConfiguration.stationTopic)
+                .toTable();
+
         val stream: KStream<String, WeatherInformationEvent> = streamsBuilder.stream(labConfiguration.weatherTopic)
 
         stream.filter { id, weatherInformationEvent ->  weatherInformationEvent.temperature < 100}
+            .join(stationKTable
+            ) { weatherInfo, tableEntry ->
+                AggregatedWeatherInformationEvent(
+                    tableEntry.stationId,
+                    tableEntry.name,
+                    tableEntry.location,
+                    weatherInfo.temperature,
+                    weatherInfo.humidity
+                )
+            }
             .to(labConfiguration.weatherProcessedTopic)
     }
 }
